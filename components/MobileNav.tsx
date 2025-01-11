151b23abb1be0ea2c46bf6ec93d8608c26a72867
Ro <rosinaaa@proton.me>
2025-01-11 17:12:16 +0000

    Build mobile navigation with shadcn sheet

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,7 +1,78 @@
 "use client";
 
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "services",
+    path: "/services",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
+
 const MobileNav = () => {
-  return <nav>MobileNav</nav>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent aria-describedby="mobile-nav-description">
+        <SheetTitle className="sr-only"></SheetTitle>
+        <SheetDescription className="sr-only"></SheetDescription>
+        {/* Logo */}
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Luke<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        {/* Nav */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, i) => {
+            return (
+              <Link
+                href={link.path}
+                key={i}
+                className={`${
+                  link.path === pathname
+                    ? "text-accent border-b-2 border-accent"
+                    : ""
+                } text-xl capitalize hover:text-accent transition-all}`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
